51ea51a189293cc36a9175b1d59149c24a3372f9
Kazuki <ibnu235729@gmail.com>
2022-03-16 21:03:48 +0700

    Update Version 1.0.0

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,5 +2,6 @@
 @section('container')
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
     <h1 class="h3 mb-0 text-gray-800">Welcome Back Admin, {{ Auth::user()->name }}</h1>
+    <small class="badge bg-primary text-white">version {{ config('app.version') }}</small>
 </div>
 @endsection
